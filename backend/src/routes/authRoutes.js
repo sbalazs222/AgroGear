21445d8c160560e72e0d90d8d8831e8bd2184466
= <=>
2026-01-28 13:06:08 +0100

    added middleware to auth routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -5,8 +5,8 @@ import { authenticateToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateFieldCount(3), validateRequiredFields(['username', 'email', 'password']), register);
+router.post("/login", validateFieldCount(2), validateRequiredFields(['email', 'password']), login);
 router.post("/logout", authenticateToken, logout);
 
 export default router;
