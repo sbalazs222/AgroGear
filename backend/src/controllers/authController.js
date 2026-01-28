03e9f48caea7e514aeaafe32d36362642319c26d
= <=>
2026-01-28 13:00:39 +0100

    added register logic

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -17,7 +17,7 @@ export async function register(req, res, next){
       return res.status(409).json({ message: "Email or username already in use" });
     }
     const hashedPassword = await argon2.hash(password);
-    await pool.query('INSERT INTO users (email, username, password) VALUES (?, ?, ?)', [email, username, hashedPassword]);
+    await pool.query('INSERT INTO users (email, username, password_hash) VALUES (?, ?, ?)', [email, username, hashedPassword]);
     res.status(201).json({ message: "User registered successfully" });
   }
   catch(err){
@@ -26,7 +26,25 @@ export async function register(req, res, next){
 }
 
 export async function login(req, res, next){
-
+  const {email, password} = req.body;
+  if (typeof email !== "string" || typeof password !== "string") {
+    return res.status(400).json({ message: "Invalid input" });
+  }
+  try {
+    const [user] = await pool.query('SELECT id, username, password_hash FROM users WHERE email = ?', [email]);
+    if (user.length === 0) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    if (!await argon2.verify(user[0].password_hash, password)) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    const token = generateToken({ id: user[0].id, username: user[0].username });
+    res.cookie("token", token, { httpOnly: true, secure: false, sameSite: 'strict' });
+    res.status(200).json({ message: "Login successful" });
+  }
+  catch (err) {
+    next(err);
+  }
 }
 
 export async function logout(req, res, next){
