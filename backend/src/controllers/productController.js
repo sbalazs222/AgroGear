89d90c32b54427c75e02cfa1049ac5dbf5be9b41
= <=>
2026-02-04 13:07:38 +0100

    added auth for query

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -4,5 +4,9 @@ export async function getProductsByCategory(req, res, next) {
     const { category } = req.params;
     const formatedCategory = category.toLowerCase().trim();
     const [products] = await pool.query('SELECT p.id, p.name, p.description, p.price, p.stock FROM products p INNER JOIN categories c ON c.id = p.category_id WHERE magyar_trim(c.name) = ?', [formatedCategory]);
-    res.json({ products });
+    res.status(200).json({ message: "Succesful query", data: products });
+}
+
+export async function newProduct(req, res) {
+    
 }
--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getProductsByCategory } from "../controllers/productController.js";
+import { authenticateToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/category/:category", getProductsByCategory);
+router.get("/category/:category", authenticateToken, getProductsByCategory);
 
 export default router;
