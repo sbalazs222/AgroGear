7d54f6287c00d3215efa9c97469415b4dce82a5c
sbalazs222 <sbalazs222@gmail.com>
2026-02-08 20:53:36 +0100

    fix - added new function to routes

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getProductsByCategory } from "../controllers/productController.js";
+import { validateRequiredFields, validateFieldCount } from "psgutil";
+import { getProductsByCategory, newProduct } from "../controllers/productController.js";
 import { authenticateToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.get("/category/:category", authenticateToken, getProductsByCategory);
-
+router.post("/", authenticateToken, validateRequiredFields(["name", "description", "price", "stock", "category"]), validateFieldCount(5), newProduct);
 export default router;
