41cb7895f8d899200bbe047dc38034f533e3d961
sbalazs222 <sbalazs222@gmail.com>
2026-03-03 18:21:56 +0100

    added route for orders

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,9 +2,11 @@ import express from "express";
 
 import { authenticateToken } from "../middlewares/auth.js";
 import { getUserData } from "../controllers/userController.js";
+import { getOrders } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.get("/data", authenticateToken, getUserData);
+router.get("/orders", authenticateToken, getOrders);
 
 export default router;
