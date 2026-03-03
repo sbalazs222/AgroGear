e6a24c226bc5784e49599fe663b7e40594ec2368
sbalazs222 <sbalazs222@gmail.com>
2026-03-03 23:08:48 +0100

    added validation for favs

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,12 +2,13 @@ import express from "express";
 
 import { authenticateToken } from "../middlewares/auth.js";
 import { getUserData, getOrders, getFavourites, setFavourite  } from "../controllers/userController.js";
+import { validateFieldCount, validateRequiredFields } from "psgutil";
 
 const router = express.Router();
 
 router.get("/data", authenticateToken, getUserData);
 router.get("/orders", authenticateToken, getOrders);
 router.get("/favourites", authenticateToken, getFavourites);
-router.post("/favourites", authenticateToken, setFavourite);
+router.post("/favourites", authenticateToken, validateFieldCount(1), validateRequiredFields(["productId"]), setFavourite);
 
 export default router;
